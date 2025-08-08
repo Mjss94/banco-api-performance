ca5effa64f19155348ce147506e9756e8a79ad82
Mary Jacy <84360966+Mjss94@users.noreply.github.com>
2025-08-07 22:44:33 -0300

    Adcionei fixtures para os dados de entrada do teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,44 +1,43 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
+  
   stages: [
     { duration: '10s', target: 10 },
     { duration: '20s', target: 10 },
     { duration: '10s', target: 30 },
     { duration: '20s', target: 30 },
     { duration: '20s', target: 0 },
-
-
-
   ],
+ 
+  iterations: 1,
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01']
   }
 }
-
 export default function () {
-   const url = 'http://localhost:3000/login';
+  const url = 'http://localhost:3000/login';
 
-   const payload = JSON.stringify({
-    username: 'julio.lima',
-    senha: '123456',
-   });
+  postLogin.username = "junior.lima"
+  console.log(postLogin)
+  const payload = JSON.stringify(postLogin);
 
-   const params = {
+  const params = {
     headers: {
       'Content-Type': 'application/json',
     },
-   };
+  };
 
-   const res = http.post(url, payload, params);
+  const res = http.post(url, payload, params);
 
-   check ( res, {
-     'Validar que o Status é 200': (r) => r.status === 200,
-     'Validar que o token é string': (r) => typeof (r.json().token) == 'string'
+  check(res, {
+    'Validar que o Status é 200': (r) => r.status === 200,
+    'Validar que o token é string': (r) => typeof (r.json().token) == 'string'
 
-   })
+  })
 
-    sleep(1);
+  sleep(1);
 }
