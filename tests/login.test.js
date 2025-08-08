0a13f467ed4181a7840792a8c669f81545789220
Mary Jacy <84360966+Mjss94@users.noreply.github.com>
2025-08-08 00:25:00 -0300

    Criei um arquivo de configuracao e apliquei variaveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { pegarBaseURL } from '../utils/variaveis.js'
+
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 export const options = {
@@ -12,17 +14,16 @@ export const options = {
     { duration: '20s', target: 0 },
   ],
  
-  iterations: 1,
   thresholds: {
     http_req_duration: ['p(90)<3000', 'max<5000'],
     http_req_failed: ['rate<0.01']
   }
 }
 export default function () {
-  const url = 'http://localhost:3000/login';
+  const url = pegarBaseURL() + '/login';
 
   postLogin.username = "junior.lima"
-  console.log(postLogin)
+
   const payload = JSON.stringify(postLogin);
 
   const params = {
